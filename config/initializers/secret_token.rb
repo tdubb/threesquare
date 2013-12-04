a84555fb12cafa39cc9378e5f7b455c3b71c5c29
MakerSquare Student <students@themakersquare.com>
2013-12-04 13:29:47 -0600

    Initial Repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Threesquare::Application.config.secret_key_base = '3462617960c2588ffc58b56e162f3a9a4cbb914ffd7109a606258a03da9f10eddf58fb000f156b206cd1219e46b4ab376ac740512e23c9a0d9a5d6f64d9f5a9e'
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Threesquare::Application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
+
+  # Example of regular route:
+  #   get 'products/:id' => 'catalog#view'
+
+ root 'locations#index'
+
+ resources :locations
+end
